d9adb493f80ae9453bf7b277a31eeb8bac6ba8a7
Katie Moosman <moosman.katie@gmail.com>
2019-03-11 06:31:49 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 require_relative '../../config/environment'
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
@@ -11,7 +13,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    binding.pry
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
   end
 
   get '/account' do
@@ -24,4 +33,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,9 @@
 class Helpers
+
+  def self.current_user(session)
+    user = User.find(sessino[:id].to_i)
+    
+  end
+
+  
 end
