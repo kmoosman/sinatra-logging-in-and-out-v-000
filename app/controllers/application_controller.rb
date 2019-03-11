f04143527728bb86e6ec19d6ef7f64b9fad78ec6
Katie Moosman <moosman.katie@gmail.com>
2019-03-11 06:36:19 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
   post '/login' do
 
     @user = User.find_by(username: params[:username])
-    @user.save
+    binding.pry
     if @user
       session[:user_id] = @user.id
       redirect to '/account'
