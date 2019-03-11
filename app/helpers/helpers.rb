7aefdc6d40594b3e1b12f8c81a0c7a9be56617b1
Katie Moosman <moosman.katie@gmail.com>
2019-03-11 06:32:56 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,9 +1,10 @@
 class Helpers
-
+  
   def self.current_user(session)
-    user = User.find(sessino[:id].to_i)
-    
+    @user = User.find(session[:user_id])
   end
 
-  
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+  end
 end
